18d0e7cae58190311523b4d60a5fa9291765f351
junah201 <junah.dev@gmail.com>
2023-01-07 02:28:05 +0900

    feat : Header components 제작 완료
    Header components 제작 완료

--- Frontend/src/components/base/Header.js
+++ Frontend/src/components/base/Header.js
@@ -1,14 +1,43 @@
 import React from "react";
+import styled from "styled-components";
 
 import longLogo from "../../static/images/long_logo.png";
 
+const StyledHeader = styled.header`
+	background-color: #ffffff;
+`;
+
+const StyledNav = styled.nav`
+	height: 100px;
+	box-shadow: 0px 4px 20px -20px gray;
+	display: flex;
+	justify-items: center;
+	align-items: center;
+	justify-content: center;
+	padding 0 200px;
+	border-top: 1px solid #454545;
+`;
+
+const StyledUl = styled.ul`
+	display: flex;
+	width: 900px;
+	justify-content: space-between;
+`;
+
+const StyledLi = styled.li`
+	display: inline-block;
+
+	& a {
+		font-size: 30px;
+		font-weight: 500;
+		text-decoration: none;
+		color: #000000;
+	}
+`;
+
 const Header = () => {
 	return (
-		<header
-			style={{
-				backgroundColor: "#ffffff",
-			}}
-		>
+		<StyledHeader>
 			<div
 				style={{
 					display: "flex",
@@ -19,13 +48,24 @@ const Header = () => {
 					<img src={longLogo} width="300px" alt="미래의학연구재단" />
 				</a>
 			</div>
-			<nav
-				style={{
-					height: "100px",
-					boxShadow: "0px 4px  20px 0px gray",
-				}}
-			></nav>
-		</header>
+			<StyledNav>
+				<StyledUl>
+					<HeaderNavLi link="/introduction/founder" text="재단소개" />
+					<HeaderNavLi link="/support/instructions" text="후원안내" />
+					<HeaderNavLi link="/programs" text="사업소개" />
+					<HeaderNavLi link="/news/board" text="재단소식" />
+					<HeaderNavLi link="/introduction/founder" text="알림마당" />
+				</StyledUl>
+			</StyledNav>
+		</StyledHeader>
+	);
+};
+
+const HeaderNavLi = ({ link, text }) => {
+	return (
+		<StyledLi>
+			<a href={link}>{text}</a>
+		</StyledLi>
 	);
 };
 
