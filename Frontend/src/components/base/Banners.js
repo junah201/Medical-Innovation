b17c0caa999f092c00422568c5d5a90f51707ca0
junah201 <junah.dev@gmail.com>
2023-01-08 22:13:28 +0900

    feat : 하단 배너 제작 완료
    - 하단 배너가 부드럽게 움직이도록 수정

--- Frontend/src/components/base/Banners.js
+++ Frontend/src/components/base/Banners.js
@@ -1,16 +1,30 @@
-import React, { useState } from "react";
-import styled from "styled-components";
+import React, { useState, useEffect } from "react";
+import styled, { keyframes } from "styled-components";
+
+const move = (x) => keyframes`
+	100%{
+		transform: translateX(-${x + 230}px);
+	}
+`;
+
+const StyledBannerContainer = styled.div`
+	width: 1580px;
+	height: 250px;
+	overflow: hidden;
+	background-color: #ffffff;
+`;
 
 const StyledBanners = styled.div`
+	position: relative;
 	background-color: #ffffff;
 	display: grid;
-	width: 1580px;
 	grid-template-rows: repeat(2, 110px);
 	grid-template-columns: repeat(10, 200px);
 	grid-auto-flow: column;
 	grid-gap: 30px;
-	overflow: hidden;
-	margin-right: ${(props) => props.marginLeft || "0"}px;
+	transform: translateX(-${(props) => props.x || 0}px);
+	animation: ${(props) => move(props.x)} 5s;
+	animation-delay: 1s;
 `;
 
 const dumpData = [
@@ -45,116 +59,120 @@ const dumpData = [
 		link: "https://www.organon.com/",
 	},
 	{
-		name: "organon",
-		path: "/images/banner/pfizer.png",
-		link: "https://www.organon.com/",
+		name: "abbott",
+		path: "/images/banner/Abbott.png",
+		link: "https://www.abbott.com/",
+	},
+	{
+		name: "bbraun",
+		path: "/images/banner/B.Braun.png",
+		link: "https://www.bbraun.com/",
+	},
+	{
+		name: "gccorp",
+		path: "/images/banner/GC녹십자.png",
+		link: "http://www.gccorp.com/",
+	},
+	{
+		name: "medtronic",
+		path: "/images/banner/Medtronic.png",
+		link: "https://europe.medtronic.com/",
+	},
+	{
+		name: "microport",
+		path: "/images/banner/MicroPort.png",
+		link: "https://microport.com/",
 	},
 	{
 		name: "organon",
-		path: "/images/banner/pfizer.png",
+		path: "/images/banner/Organon.png",
 		link: "https://www.organon.com/",
 	},
 	{
-		name: "organon",
-		path: "/images/banner/pfizer.png",
-		link: "https://www.organon.com/",
+		name: "abbott",
+		path: "/images/banner/Abbott.png",
+		link: "https://www.abbott.com/",
 	},
 	{
-		name: "organon",
-		path: "/images/banner/pfizer.png",
-		link: "https://www.organon.com/",
+		name: "bbraun",
+		path: "/images/banner/B.Braun.png",
+		link: "https://www.bbraun.com/",
 	},
 	{
-		name: "organon",
-		path: "/images/banner/pfizer.png",
-		link: "https://www.organon.com/",
+		name: "gccorp",
+		path: "/images/banner/GC녹십자.png",
+		link: "http://www.gccorp.com/",
 	},
 	{
-		name: "organon",
-		path: "/images/banner/pfizer.png",
-		link: "https://www.organon.com/",
+		name: "medtronic",
+		path: "/images/banner/Medtronic.png",
+		link: "https://europe.medtronic.com/",
 	},
 	{
-		name: "organon",
-		path: "/images/banner/pfizer.png",
-		link: "https://www.organon.com/",
+		name: "microport",
+		path: "/images/banner/MicroPort.png",
+		link: "https://microport.com/",
 	},
 	{
-		name: "organon",
-		path: "/images/banner/pfizer.png",
-		link: "https://www.organon.com/",
+		name: "abbott",
+		path: "/images/banner/Abbott.png",
+		link: "https://www.abbott.com/",
 	},
 	{
-		name: "organon",
-		path: "/images/banner/pfizer.png",
-		link: "https://www.organon.com/",
+		name: "bbraun",
+		path: "/images/banner/B.Braun.png",
+		link: "https://www.bbraun.com/",
 	},
 	{
-		name: "organon",
-		path: "/images/banner/pfizer.png",
-		link: "https://www.organon.com/",
+		name: "gccorp",
+		path: "/images/banner/GC녹십자.png",
+		link: "http://www.gccorp.com/",
 	},
 	{
-		name: "organon",
-		path: "/images/banner/pfizer.png",
-		link: "https://www.organon.com/",
-	},
-	{
-		name: "organon",
-		path: "/images/banner/pfizer.png",
-		link: "https://www.organon.com/",
-	},
-	{
-		name: "organon",
-		path: "/images/banner/pfizer.png",
-		link: "https://www.organon.com/",
-	},
-	{
-		name: "organon",
-		path: "/images/banner/pfizer.png",
-		link: "https://www.organon.com/",
-	},
-	{
-		name: "organon",
-		path: "/images/banner/pfizer.png",
-		link: "https://www.organon.com/",
-	},
-	{
-		name: "organon",
-		path: "/images/banner/pfizer.png",
-		link: "https://www.organon.com/",
+		name: "medtronic",
+		path: "/images/banner/Medtronic.png",
+		link: "https://europe.medtronic.com/",
 	},
 ];
 
 const Banners = () => {
 	const [bannerIndex, setBannerIndex] = useState(0);
 
-	console.log("재랜더링");
+	useEffect(() => {
+		const mover = setInterval(() => {
+			if (bannerIndex < dumpData.length / 2 - 8) {
+				setBannerIndex(bannerIndex + 1);
+			} else {
+				setBannerIndex(0);
+			}
+		}, 6000);
 
-	const onNextClickHandler = () => {
-		if (bannerIndex < dumpData.length / 2 - 1) {
-			setBannerIndex(bannerIndex + 1);
-		}
-		console.log(bannerIndex);
-	};
+		return () => clearInterval(mover);
+	});
 
 	return (
 		<>
-			<button onClick={onNextClickHandler}>다음</button>
-			<StyledBanners marginLeft={bannerIndex * 210}>
-				{dumpData.map((item, index) => {
-					return (
-						<a href={item.link} key={index}>
-							<img
-								width="200px"
-								height="110px"
-								src={item.path}
-								alt={item.name}
-							/>
-						</a>
-					);
-				})}
-			</StyledBanners>
+			<StyledBannerContainer>
+				<StyledBanners x={bannerIndex * 230}>
+					{dumpData.map((item, index) => {
+						return (
+							<a
+								href={item.link}
+								key={index}
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<img
+									width="200px"
+									height="110px"
+									src={item.path}
+									alt={item.name}
+								/>
+							</a>
+						);
+					})}
+				</StyledBanners>
+			</StyledBannerContainer>
 		</>
 	);
 };
