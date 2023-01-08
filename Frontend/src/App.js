49cee660b7f795a63dca5871f0364b170596a660
junah201 <junah.dev@gmail.com>
2023-01-09 02:19:21 +0900

    feat : 개인정보처리방침 페이지

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/base/Header";
 import Footer from "./components/base/Footer";
 
 import HomePage from "./pages/HomePage";
+import PrivacyPolicyPage from "./pages/PrivacyPolicyPage";
 
 function App() {
 	return (
@@ -12,6 +13,7 @@ function App() {
 			<Header />
 			<Routes>
 				<Route path="/" element={<HomePage />} />
+				<Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
 				<Route path="*" element={<h1>404</h1>} />
 			</Routes>
 			<Footer />
