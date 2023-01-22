c61b202490a60f3213480b977928fbd317feb564
junah201 <junah.dev@gmail.com>
2023-01-22 20:51:28 +0900

    feat : 네이버 소유 확인 증명용 meta tag 추가

--- Frontend/src/components/base/Footer.jsx
+++ Frontend/src/components/base/Footer.jsx
@@ -143,7 +143,7 @@ const Footer = () => {
 			<StyledBottomFooter>
 				<a href="/privacy-policy">개인정보처리방침</a>
 				<p>(03080) 서울시 종로구 대학로 101, 12634호</p>
-				<p>TEL : 02-2072-2226</p>
+				<p>전화번호 : 02-2072-2226</p>
 				<p>Copyright(c) 2016 재단법인 미래의학연구재단. All rights reserved.</p>
 			</StyledBottomFooter>
 		</StyledFooter>
--- Frontend/src/pages/error/NotFoundPage.jsx
+++ Frontend/src/pages/error/NotFoundPage.jsx
@@ -6,6 +6,7 @@ import Message from "../../components/common/Message";
 function NotFoundPage() {
 	return (
 		<Page title="404 Not Found">
+			<h1>404 Not Found</h1>
 			<Message>
 				해당 페이지를 찾을 수 없습니다. 잘못된 URL이거나, 개발 중인 페이지일 수
 				있습니다.
