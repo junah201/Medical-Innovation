31c816f65f6a09816cc3e4d4654fd41578ad0d4d
junah201 <junah.dev@gmail.com>
2023-02-23 20:43:52 +0900

    feat : 게시물 업로드 에디터

--- Frontend/src/pages/admin/PostUploadPage.jsx
+++ Frontend/src/pages/admin/PostUploadPage.jsx
@@ -4,11 +4,14 @@ import styled from "styled-components";
 import axios from "axios";
 
 import AdminPage from "../../components/admin/AdminPage";
-import { API_URL } from "../../utils/const";
+import { API_URL, S3_URL } from "../../utils/const";
 import AuthContext from "../../context/AuthContext";
 import Message from "../../components/common/Message";
 import PostContent from "../../components/post/PostContent";
 import { useNavigate } from "react-router-dom";
+
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
 const StyledPostUploadPage = styled.div`
 	display: flex;
@@ -21,6 +24,10 @@ const StyledPostUploadPage = styled.div`
 	& form {
 		display: flex;
 		flex-direction: column;
+	}
+
+	& .ck-editor__editable_inline {
+		min-height: 600px;
 	}
 
 	& input,
@@ -82,42 +89,6 @@ const PostUploadPage = () => {
 	const [content, setContent] = useState("");
 	const [files, setFiles] = useState([]);
 
-	const [imgSize, setImgSize] = useState("30%");
-	const [imgSort, setImgSort] = useState("center");
-
-	const [link, setLink] = useState("");
-	const [linkPreview, setLinkPreview] = useState("");
-
-	const handleImgaeUpload = (e) => {
-		e.preventDefault();
-
-		const formData = new FormData();
-		formData.append("file", e.target.files[0]);
-
-		axios({
-			url: `${API_URL}/api/v1/file/upload`,
-			method: "POST",
-			headers: {
-				accept: "application/json",
-				"Content-Type": "multipart/form-data",
-				Authorization: `Bearer ${authCtx.accessToken}`,
-			},
-			data: formData,
-		}).then((res) => {
-			setContent((prev) => {
-				return `${prev}\n\n<div class="img-container ${imgSort}"><img width = ${imgSize} src="https://medical-innovation.s3.ap-northeast-2.amazonaws.com/upload/${res.data.filename}" /></div>\n\n`;
-			});
-		});
-	};
-
-	const handleLinkUpload = (e) => {
-		e.preventDefault();
-
-		setContent((prev) => {
-			return `${prev} <a href="${link}">${linkPreview}</a> `;
-		});
-	};
-
 	const handleSubmit = (e) => {
 		e.preventDefault();
 
@@ -157,11 +128,48 @@ const PostUploadPage = () => {
 		});
 	};
 
+	const customUploadAdapter = (loader) => {
+		return {
+			upload() {
+				return new Promise((resolve, reject) => {
+					const data = new FormData();
+					loader.file.then((file) => {
+						data.append("file", file);
+
+						axios({
+							url: `${API_URL}/api/v1/file/upload`,
+							method: "POST",
+							headers: {
+								accept: "application/json",
+								"Content-Type": "multipart/form-data",
+								Authorization: `Bearer ${authCtx.accessToken}`,
+							},
+							data: data,
+						})
+							.then((res) => {
+								resolve({
+									default: `${S3_URL}/upload/${res.data.filename}`,
+								});
+							})
+							.catch((err) => reject(err));
+					});
+				});
+			},
+		};
+	};
+
+	function uploadPlugin(editor) {
+		editor.plugins.get("FileRepository").createUploadAdapter = (loader) => {
+			return customUploadAdapter(loader);
+		};
+	}
+
 	return (
 		<AdminPage>
 			<StyledPostUploadPage>
 				<h1>게시물 업로드</h1>
 				<Message>{errorMessages}</Message>
+
 				<form onSubmit={handleSubmit}>
 					<input
 						type="text"
@@ -188,101 +196,28 @@ const PostUploadPage = () => {
 						})}
 					</select>
 					<br />
-					<textarea
-						type="text"
-						placeholder="Content"
-						value={content}
-						onChange={(e) => {
-							setContent(e.target.value);
+					<CKEditor
+						editor={ClassicEditor}
+						data=""
+						config={{
+							extraPlugins: [uploadPlugin],
+						}}
+						onReady={(editor) => {
+							// You can store the "editor" and use when it is needed.
+							console.log("Editor is ready to use!", editor);
+						}}
+						onChange={(event, editor) => {
+							const data = editor.getData();
+							console.log({ event, editor, data });
+							setContent(data);
+						}}
+						onBlur={(event, editor) => {
+							console.log("Blur.", editor);
+						}}
+						onFocus={(event, editor) => {
+							console.log("Focus.", editor);
 						}}
 					/>
-					<br />
-					<br />
-					<div>
-						<label>이미지 첨부</label>
-						<br />
-						<label>이미지 크기</label>{" "}
-						<input
-							type="radio"
-							name="imgSize"
-							value="30%"
-							checked={imgSize === "30%"}
-							onChange={() => setImgSize("30%")}
-						/>
-						작음
-						<input
-							type="radio"
-							name="imgSize"
-							value="50%"
-							checked={imgSize === "50%"}
-							onChange={() => setImgSize("50%")}
-						/>
-						중간
-						<input
-							type="radio"
-							name="imgSize"
-							value="100%"
-							checked={imgSize === "100%"}
-							onChange={() => setImgSize("100%")}
-						/>
-						큼
-						<br />
-						<label>이미지 정렬</label>{" "}
-						<input
-							type="radio"
-							name="imgSort"
-							value="left"
-							checked={imgSort === "left"}
-							onChange={() => setImgSort("left")}
-						/>
-						<label>왼쪽</label>
-						<input
-							type="radio"
-							name="imgSort"
-							value="center"
-							checked={imgSort === "center"}
-							onChange={() => setImgSort("center")}
-						/>
-						<label>중앙</label>
-						<input
-							type="radio"
-							name="imgSort"
-							value="right"
-							checked={imgSort === "right"}
-							onChange={() => setImgSort("right")}
-						/>
-						<label>오른쪽</label>
-						<br />
-						<input
-							type="file"
-							onChange={handleImgaeUpload}
-							accept="image/png, image/gif, image/jpeg, image/jpg"
-						/>
-					</div>
-					<br />
-					<div>
-						<label>링크 첨부</label>
-						<br />
-						<input
-							type="text"
-							placeholder="링크"
-							value={link}
-							onChange={(e) => {
-								setLink(e.target.value);
-							}}
-						/>
-						<br />
-						<input
-							type="text"
-							placeholder="미리보기 텍스트"
-							value={linkPreview}
-							onChange={(e) => {
-								setLinkPreview(e.target.value);
-							}}
-						/>
-						<br />
-						<button onClick={handleLinkUpload}>링크 추가</button>
-					</div>
 					<br />
 					<div>
 						<label>하단 첨부파일 (한번에 여러개 선택)</label>
@@ -295,7 +230,6 @@ const PostUploadPage = () => {
 							multiple
 						/>
 					</div>
-
 					<br />
 					<br />
 					<button type="submit">업로드</button>
