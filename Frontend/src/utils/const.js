3ef8bbdd04f64dbc3edc68369c63d31251ddaa32
junah201 <junah.dev@gmail.com>
2023-03-10 23:15:59 +0900

    fix : CDN 링크 수정

--- Frontend/src/utils/const.js
+++ Frontend/src/utils/const.js
@@ -2,4 +2,3 @@ export const API_URL =
 	"https://port-0-medical-innovation-3a9t2bldt5vor1.sel3.cloudtype.app";
 
 export const CDN_URL = "https://d2zkiuln09byg.cloudfront.net";
-	"https://medical-innovation.s3.ap-northeast-2.amazonaws.com";
