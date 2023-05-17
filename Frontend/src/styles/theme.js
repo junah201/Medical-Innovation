37c831941b74f6686f7ac94cd2a847d06dbdfbb3
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-05-18 01:17:08 +0900

    chore: theme 객체 속성 추가

--- Frontend/src/styles/theme.js
+++ Frontend/src/styles/theme.js
@@ -3,4 +3,7 @@ export const theme = {
   background: '#f9f9f9',
   borderColor: '#ececec',
   anchorColor: '#0000ff',
+  boxShadoxOption: '0px 4px 5px rgba(0, 0, 0, 0.015)',
+  borderOption: '1px solid #eaeaea',
+  transitionOption: 'ease-in-out 0.15s',
 };
