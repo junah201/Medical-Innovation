81be86c42fd2541ac58a55d3a2638af86d141f64
junah201 <junah.dev@gmail.com>
2023-06-17 19:30:29 +0900

    feat: layout 및 mediaQuery 상수 추가

---
+++ Frontend/src/constants/layout.ts
@@ -0,0 +1,11 @@
+export const HEADER = Object.freeze({
+  DESKTOP_HEIGHT: '100px',
+  DESKTOP_FONT_SIZE: '20px',
+  MOBILE_HEIGHT: '50px',
+  MOBILE_FONT_SIZE: '12px',
+});
+
+export const FOOTER = Object.freeze({
+  DESKTOP_HEIGHT: '200px',
+  MOBILE_HEIGHT: '200px',
+});
