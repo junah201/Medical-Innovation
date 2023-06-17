12e25a6c8c4e9dac9dc03ee9993d54d5e7adbd50
junah201 <junah.dev@gmail.com>
2023-06-17 16:25:42 +0900

    feat: eslint 초기 설정

--- Frontend/.eslintrc.cjs
+++ Frontend/.eslintrc.cjs
@@ -1,14 +1,48 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-  },
-}
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:react/recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:import/recommended",
+	],
+	overrides: [],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	plugins: ["react", "react-hooks", "@typescript-eslint", "prettier", "import"],
+	rules: {
+		"react/jsx-uses-react": "off",
+		"react/react-in-jsx-scope": "off",
+		"import/order": [
+			"error",
+			{
+				groups: [
+					["builtin", "external"],
+					["internal"],
+					["parent", "sibling", "index"],
+				],
+				"newlines-between": "always",
+				alphabetize: { order: "asc", caseInsensitive: true },
+			},
+		],
+		quotes: ["error", "single"],
+		"no-duplicate-imports": "error",
+		"no-console": ["warn", { allow: ["warn", "error", "info"] }],
+		"no-unused-vars": "warn",
+		"no-multiple-empty-lines": "warn",
+	},
+	settings: {
+		"import/resolver": {
+			typescript: {},
+		},
+		react: {
+			version: "detect",
+		},
+	},
+};
