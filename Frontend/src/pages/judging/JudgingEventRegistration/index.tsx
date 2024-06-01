e9e2fee0e647b357a20fc7c637a24525c6aab0f3
junah201 <junah.dev@gmail.com>
2024-06-01 23:35:21 +0900

    fix: 심사 행사 신청기간에만 신청 가능하도록 수정

--- Frontend/src/pages/judging/JudgingEventRegistration/index.tsx
+++ Frontend/src/pages/judging/JudgingEventRegistration/index.tsx
@@ -1,12 +1,14 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 import { getJudgingEventById } from '@/api';
 import { useCustomQuery } from '@/libs/Query';
+import { Toast } from '@/libs/Toast';
 
 import Form from './Form';
 import Info from './Info';
 
 export const JudgingEventRegistration = () => {
+  const navigate = useNavigate();
   const params = useParams() as {
     [key: string]: string;
   };
@@ -16,6 +18,20 @@ export const JudgingEventRegistration = () => {
     () => getJudgingEventById(params.event_id),
     {
       retry: 0,
+      onSuccess: (res) => {
+        const now = new Date().toISOString().slice(0, 10);
+
+        if (res.data.join_start_date > now) {
+          Toast('아직 신청 기간이 아닙니다.', 'error');
+          navigate(-1);
+          return;
+        }
+        if (res.data.join_end_date < now) {
+          Toast('신청 기간이 종료되었습니다.', 'error');
+          navigate(-1);
+          return;
+        }
+      },
     }
   );
 
